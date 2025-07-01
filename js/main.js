b241c414c986d7054d07573b37880b87fe11c3c4
Chungshu224 <chungshu224@gmail.com>
2025-07-02 01:55:59 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // 1. Mapbox Token & 資料設定
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2h1bmdzaHVsZWUiLCJhIjoiY21ja3B0NHBzMDBxMzJpc2Q3b2Zzam9qYSJ9.9gYjm_VnBH7MYA-e9zKkCw';
-let currentStyle = 'mapbox://styles/mapbox/outdoors-v12';
+let currentStyle = 'mapbox://styles/mapbox/satellite-v9';
 const styleMap = {
   satellite: 'mapbox://styles/mapbox/satellite-v9',
   terrain: 'mapbox://styles/mapbox/outdoors-v12'
